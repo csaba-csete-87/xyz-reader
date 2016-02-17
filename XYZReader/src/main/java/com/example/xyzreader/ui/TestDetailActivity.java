e88703751b4624440a73dfe61c144c717accd162
Csaba Csete <csaba.csete@gmail.com>
2016-02-17 16:54:07 +0200

    worked on details activity

--- XYZReader/src/main/java/com/example/xyzreader/ui/TestDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/TestDetailActivity.java
@@ -3,161 +3,238 @@ package com.example.xyzreader.ui;
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.format.DateUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.ArticleLoader;
 import com.example.xyzreader.data.ItemsContract;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.assist.FailReason;
-import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 public class TestDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    private Cursor mCursor;
-    private long mStartId;
+  private Cursor mCursor;
+  private long mStartId;
 
-    @Bind(R.id.article_container)
-    LinearLayout articleContainer;
+  private long mSelectedItemId;
+  private int mSelectedItemUpButtonFloor = Integer.MAX_VALUE;
 
-    private long mSelectedItemId;
-    private int mSelectedItemUpButtonFloor = Integer.MAX_VALUE;
+  private SimpleImageLoadingListener articleHeaderImageLoadingListener = new SimpleImageLoadingListener() {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_detail);
-        ButterKnife.bind(this);
-        ActivityCompat.postponeEnterTransition(this);
+    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+      ActivityCompat.startPostponedEnterTransition(TestDetailActivity.this);
+    }
 
-        getLoaderManager().initLoader(0, null, this);
-        if (savedInstanceState == null) {
-            if (getIntent() != null && getIntent().getData() != null) {
-                mStartId = ItemsContract.Items.getItemId(getIntent().getData());
-                mSelectedItemId = mStartId;
-            }
+  };
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_test_detail);
+    ActivityCompat.postponeEnterTransition(this);
+
+    setupToolbar();
+
+    getLoaderManager().initLoader(0, null, this);
+    if (savedInstanceState == null) {
+      if (getIntent() != null && getIntent().getData() != null) {
+        mStartId = ItemsContract.Items.getItemId(getIntent().getData());
+        mSelectedItemId = mStartId;
+      }
+    }
+  }
+
+  private void setupToolbar() {
+    setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override
+  public void onBackPressed() {
+    finish();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.detail, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == R.id.action_share) {
+      Intent shareIntent = getShareIntent();
+      startActivity(shareIntent);
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  @NonNull
+  private Intent getShareIntent() {
+    Intent sendIntent = new Intent();
+    sendIntent.setAction(Intent.ACTION_SEND);
+    sendIntent.putExtra(Intent.EXTRA_TEXT, getFullArticle());
+    sendIntent.setType("text/plain");
+    return sendIntent;
+  }
+
+  @Override
+  public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+    return ArticleLoader.newAllArticlesInstance(this);
+  }
+
+  @Override
+  public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+    setCursor(cursor);
+
+    setToolbarTitle();
+    initArticle();
+  }
+
+  private void initArticle() {
+    setHeaderImage();
+    setArticleTitle();
+    setArticleSubtitle();
+    setArticleBody();
+  }
+
+  @Override
+  public void onEnterAnimationComplete() {
+    super.onEnterAnimationComplete();
+    Animator animator = getUpwardScrollAnimator();
+    animator.start();
+  }
+
+  @NonNull
+  private ObjectAnimator getUpwardScrollAnimator() {
+    return ObjectAnimator.ofInt(
+        findViewById(R.id.article_container),
+        "scrollY",
+        getResources().getDimensionPixelSize(R.dimen.init_scroll_up_distance),
+        0
+    ).setDuration(500);
+  }
+
+  @Override
+  public void onLoaderReset(Loader<Cursor> cursorLoader) {
+    mCursor = null;
+  }
+
+  private void setToolbarTitle() {
+    ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle(getArticleTitle());
+  }
+
+  private void setCursor(Cursor cursor) {
+    mCursor = cursor;
+
+    // Select the start ID
+    if (mStartId > 0) {
+      mCursor.moveToFirst();
+      // TODO: optimize
+      while (!mCursor.isAfterLast()) {
+        if (mCursor.getLong(ArticleLoader.Query._ID) == mStartId) {
+          final int position = mCursor.getPosition();
+          mCursor.moveToPosition(position);
+          break;
         }
+        mCursor.moveToNext();
+      }
+      mStartId = 0;
     }
+  }
 
-    @Override
-    public void onBackPressed() {
-        finish();
-//        finishAfterTransition();
-    }
+  private void setHeaderImage() {
+    ImageView iv = (ImageView) findViewById(R.id.photo);
+    ImageLoader.getInstance().displayImage(getArticlePhotoUrl(), iv, articleHeaderImageLoadingListener);
+  }
 
-    @Override
-    public boolean onNavigateUp() {
-//        finishAfterTransition();
-        finish();
-        return true;
-    }
+  private void setArticleBody() {
+    TextView body = (TextView) findViewById(R.id.article_body);
+    body.setText(Html.fromHtml(getArticlePart(ArticleLoader.Query.BODY)));
+  }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return ArticleLoader.newAllArticlesInstance(this);
-    }
+  private void setArticleSubtitle() {
+    TextView t = (TextView) findViewById(R.id.article_subtitle);
+    t.setText(Html.fromHtml(getArticleSubtitle()));
+  }
 
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        setCursor(cursor);
+  @NonNull
+  private String getArticleSubtitle() {
+    return getReadableDate()
+        + getColoredText(getArticleAuthor());
+  }
 
-        setTitle();
-        setHeaderImage();
-        setBody();
-    }
+  @NonNull
+  private String getColoredText(String text) {
+    return " by <font color='#ffffff'>"
+        + text
+        + "</font>";
+  }
 
-    @Override
-    public void onEnterAnimationComplete() {
-        super.onEnterAnimationComplete();
-        final int startScrollPos = getResources().getDimensionPixelSize(R.dimen.init_scroll_up_distance);
-        Animator animator = ObjectAnimator.ofInt(
-                articleContainer,
-                "scrollY",
-                startScrollPos,
-                0).setDuration(500);
-        animator.start();
-    }
+  @NonNull
+  private String getReadableDate() {
+    return DateUtils.getRelativeTimeSpanString(
+        getArticleDate(),
+        System.currentTimeMillis(),
+        DateUtils.HOUR_IN_MILLIS,
+        DateUtils.FORMAT_ABBREV_ALL
+    ).toString();
+  }
 
-    private void setCursor(Cursor cursor) {
-        mCursor = cursor;
+  private long getArticleDate() {
+    return mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE);
+  }
 
-        // Select the start ID
-        if (mStartId > 0) {
-            mCursor.moveToFirst();
-            // TODO: optimize
-            while (!mCursor.isAfterLast()) {
-                if (mCursor.getLong(ArticleLoader.Query._ID) == mStartId) {
-                    final int position = mCursor.getPosition();
-                    mCursor.moveToPosition(position);
-                    break;
-                }
-                mCursor.moveToNext();
-            }
-            mStartId = 0;
-        }
-    }
+  private void setArticleTitle() {
+    TextView t = (TextView) findViewById(R.id.article_title);
+    t.setText(getArticleTitle());
+  }
 
-    private void setTitle() {
-        ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle(mCursor.getString(ArticleLoader.Query.TITLE));
-    }
+  private String getArticleTitle() {
+    return getArticlePart(ArticleLoader.Query.TITLE);
+  }
 
-    private void setHeaderImage() {
-        ImageView iv = (ImageView) findViewById(R.id.photo);
-        ImageLoader.getInstance().displayImage(mCursor.getString(ArticleLoader.Query.PHOTO_URL), iv, new ImageLoadingListener() {
-            @Override
-            public void onLoadingStarted(String imageUri, View view) {
+  private String getArticleAuthor() {
+    return getArticlePart(ArticleLoader.Query.AUTHOR);
+  }
 
-            }
+  private String getArticleBody() {
+    return getArticlePart(ArticleLoader.Query.BODY);
+  }
 
-            @Override
-            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+  private String getArticlePhotoUrl() {
+    return getArticlePart(ArticleLoader.Query.PHOTO_URL);
+  }
 
-            }
+  private String getArticlePart(int part) {
+    return mCursor.getString(part);
+  }
 
-            @Override
-            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-                ActivityCompat.startPostponedEnterTransition(TestDetailActivity.this);
-            }
+  private String getFullArticle() {
+    return getArticleTitle() + getNewLine() + getNewLine() + getArticleBody();
+  }
 
-            @Override
-            public void onLoadingCancelled(String imageUri, View view) {
-
-            }
-        });
-    }
-
-    private void setBody() {
-        TextView t = (TextView) findViewById(R.id.article_byline);
-        t.setText(Html.fromHtml(
-                DateUtils.getRelativeTimeSpanString(
-                        mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
-                        System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
-                        DateUtils.FORMAT_ABBREV_ALL).toString()
-                        + " by "
-                        + mCursor.getString(ArticleLoader.Query.AUTHOR)
-        ));
-        TextView body = (TextView) findViewById(R.id.article_body);
-        body.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY)));
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-        mCursor = null;
-    }
+  private String getNewLine() {
+    return "//n";
+  }
 
 }
